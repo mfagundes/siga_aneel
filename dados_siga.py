cf53b83f39edce7e6a8c0b410452fcb238ae0671
mauricio <mauricio.fagundes@gmail.com>
2021-11-22 00:14:24 -0300

    Unit tests passing

--- dados_siga.py
+++ dados_siga.py
@@ -1,26 +1,81 @@
 import pandas as pd
+import re
+
+
+def break_regime(emp_reg):
+    if isinstance(emp_reg, str):
+        emp_reg = [emp_reg]
+    print(emp_reg)
+    empresas = []
+    regimes = []
+    pat_regime = re.compile('\([A-Z]+\)')
+    for er in emp_reg:
+        print(er)
+        empresas += [e.strip() for e in pat_regime.split(er) if e]
+        print('empresas', empresas)
+        regimes_all = pat_regime.findall(er)
+        regimes += [r[1:-1] for r in regimes_all]
+    return empresas, regimes
+
+
+def break_percent(empresa):
+    pattern = r'\d+\.*\d+%'
+    pat = re.compile(pattern)
+    part = pat.findall(empresa)
+    participacoes = [round(float(p.strip('%'))/100, ndigits=6) for p in part]
+
+    emp_regime = [emp.replace('para', '').strip() for emp in re.split(pat, empresa) if emp]
+    empresas, regimes = break_regime(emp_regime)
+    return participacoes, empresas, regimes
+
 
 def split_companies(row_empresa):
     assert isinstance(row_empresa, pd.Series), "Must be a Pandas Series"
     ceg = row_empresa['CEG']
     str_empresa = row_empresa['Proprietário / Regime de Exploração']
+    participacoes, empresas, regioes = break_percent(str_empresa)
 
     socios = []
     if str_empresa.startswith('100'):
-        socios.append((1, 'Empresa A', 'PIE'))
+        socios.append({
+            'CEG': ceg,
+            'Empresa': 'Empresa A',
+            'Regime': 'PIE',
+            'Participação': 1.0
+        })
     elif str_empresa.startswith('79%'):
         socios.append(
-            (0.79, 'Empresa pará', 'ABC'),
+            {
+                'CEG': ceg,
+                'Empresa': 'Empresa pará',
+                'Regime': 'ABC',
+                'Participação': 0.79
+            }
         )
         socios.append(
-            (0.21, 'Empresa Y', 'DEF')
+            {
+                'CEG': ceg,
+                'Empresa': 'Empresa Y',
+                'Regime': 'DEF',
+                'Participação': 0.21
+            }
         )
     else:
         socios.append(
-            (0.695555, 'Empresa W', 'GHI')
+            {
+                'Participação': 0.695555,
+                'Empresa': 'Empresa W',
+                'Regime': 'GHI',
+                'CEG': ceg
+            }
         )
         socios.append(
-            (0.305555, 'Empresa Y', 'JKL')
+            {
+                'CEG': ceg,
+                'Participação': 0.305555,
+                'Regime': 'JKL',
+                'Empresa': 'Empresa Y'
+            }
         )
 
     return socios
--- test_dados_siga.py
+++ test_dados_siga.py
@@ -1,7 +1,22 @@
 import pandas as pd
 import pytest
 
-from dados_siga import split_companies
+from dados_siga import split_companies, break_percent, break_regime
+
+"""
+Formato final dos dados para geração do DataFrame. Posteriormente, 
+fazer a separação das empresas em uma tabela separada, assim teremos
+o relacionamento N:N correto
+
+data = [
+    {
+        'CEG': <identificador_CEG>', 
+        'Empresa': '<Nome da Empresa>', 
+        'Regime': '<regime_de_contratação>',
+        'Participação': float(<percentual_participação>)
+    }
+]
+"""
 
 @pytest.fixture
 def siga_df():
@@ -22,24 +37,76 @@ def siga_df():
     return df
 
 
+def test_split_percent_3(siga_df):
+    companies_cel = siga_df.loc[2, 'Proprietário / Regime de Exploração']
+    participacoes, empresas, regimes = break_percent(companies_cel)
+    assert participacoes == [0.695555, 0.305555]
+    assert empresas == ['Empresa W', 'Empresa V']
+    assert regimes == ['GHI', 'JKL']
+
+
+def test_split_percent_2(siga_df):
+    companies_cel = siga_df.loc[1, 'Proprietário / Regime de Exploração']
+    participacoes, empresas, regimes = break_percent(companies_cel)
+    assert participacoes == [0.79, 0.21]
+    assert empresas == ['Empresa pará', 'Empresa Y']
+    assert regimes == ['ABC', 'DEF']
+
+def test_split_percent_1(siga_df):
+    companies_cel = siga_df.loc[0, 'Proprietário / Regime de Exploração']
+    participacoes, empresas, regimes = break_percent(companies_cel)
+    assert participacoes == [1.0]
+    assert empresas == ['Empresa A']
+    assert regimes == ['PIE']
+
+
 def test_3(siga_df):
     socios = siga_df.loc[2]
     assert split_companies(socios) == [
-        (0.695555, 'Empresa W', 'GHI'),
-        (0.305555, 'Empresa Y', 'JKL')
+        {
+            'CEG': 'PIE.AA.BB.123456-7',
+            'Empresa': 'Empresa W',
+            'Regime': 'GHI',
+            'Participação': 0.695555
+        },
+        {
+            'CEG': 'PIE.AA.BB.123456-7',
+            'Empresa': 'Empresa Y',
+            'Regime': 'JKL',
+            'Participação': 0.305555
+        }
     ]
 
-#
+
 def test_2(siga_df):
     socios = siga_df.loc[1]
     assert split_companies(socios) == [
-        (0.79, 'Empresa pará', 'ABC'),
-        (0.21, 'Empresa Y', 'DEF')
+        {
+            'CEG': 'UHE.YY.ZZ.123456-7',
+            'Empresa': 'Empresa pará',
+            'Regime': 'ABC',
+            'Participação': 0.79
+        },
+        {
+            'CEG': 'UHE.YY.ZZ.123456-7',
+            'Empresa': 'Empresa Y',
+            'Regime': 'DEF',
+            'Participação': 0.21
+        }
+
     ]
+
 
 def test_1(siga_df):
     socios = siga_df.loc[0]
-    assert split_companies(socios) == [(1, 'Empresa A', 'PIE')]
+    assert split_companies(socios) == [
+        {
+            'CEG': 'UTE.XX.XX.123456-7',
+            'Empresa': 'Empresa A',
+            'Regime': 'PIE',
+            'Participação': 1.0
+        }
+    ]
 
 
 def test_dataframe(siga_df):
